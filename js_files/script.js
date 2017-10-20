7b3f74680813c700c9882f772a3acbadcdc53e0e
ErwinOudG <github@oudgenoeg.nl>
2017-10-20 11:28:44 +0200

    added files

---
+++ js_files/script.js
@@ -0,0 +1,15 @@
+// this snippet scrolls slowly to id (hash)
+// future addon animate switch
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var idHash = this.hash;
+      $('html, body').animate({
+          scrollTop: $(idHash).offset().top
+            }, 1000, function(){
+        window.location.hash = idHash;
+      });
+    }
+  });
+});
