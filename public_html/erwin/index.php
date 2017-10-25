74cfbcd269c13b22694e0f147de0183369d94958
ErwinOudG <github@oudgenoeg.nl>
2017-10-25 14:37:04 +0200

    new folder structure

---
+++ public_html/erwin/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!-- Portfolio voor codegorilla -->
+<!-- Portfolio van en door Erwin Oudgenoeg  -->
+<!-- copyrights  E.A. Oudgenoeg-->
+
+<html>
+    <head>
+        <title>Portfolio Erwin Oudgenoeg</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+
+    <body class="cl_mainarticle">
+        <?php
+        include "../../private_html/erwin/php_files/topmenu.php";
+        ?>
+    </body>
+    <script src="/js_files/script.js"></script>
+    <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+</html>
