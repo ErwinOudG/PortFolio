760ad46808d8d9c2b1eb76dc09fadee0d8f344d3
ErwinOudG <github@oudgenoeg.nl>
2017-10-26 16:48:04 +0200

    mysql menu versie 1

--- private_html/erwin/php_files/topmenu.php
+++ private_html/erwin/php_files/topmenu.php
@@ -1,4 +1,6 @@
 <?php
+include "../../private_html/dev/php_files/dbvars.php";
+$ar_menuitemname = array();
 // Create connection
 $conn =  mysqli_connect($servername, $username, $password);
 
@@ -7,16 +9,22 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT menu_item_name FROM $dbmenuitem WHERE menu_name = 'Top' ORDER BY menu_order";
+$sql = ("SELECT menu_item_name
+        FROM u12893p9221_fgb.fgb_menu_items
+        WHERE menu_name = 'Top'
+        ORDER BY menu_order")
+        ;
 $result = $conn->query($sql);
-if ($result->num_rows > 0) {
+$ar_menuitemname = mysqli_fetch_all($result, MYSQLI_NUM);
+// if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $ar_menuitemname = array('.$row[menu_item_name].');
-    }
-} else {
-    echo "0 results";
-}
+    // while($row = $result->fetch_assoc()) {
+        // $ar_menuitemname = array('.$row[menu_item_name].');
+        // echo ".$row[menu_item_name].";
+    // }
+// } else {
+    // echo "0 results";
+// }
 // $sql = "INSERT INTO ".$database." (log_time, log_ipaddr, log_port, log_subd)
 // VALUES ('".$gm_date."', '".$s_remaddr."', '".$s_remport."', '".$subdomainname."');";
 //
@@ -27,7 +35,6 @@ if ($result->num_rows > 0) {
 // }
 
 
-mysqli_close($conn);
 // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
 // echo '<li><a href="#id'.$row[menu_item_name].'">'.$row[menu_item_name].'</a></li>';
 //
@@ -39,19 +46,24 @@ mysqli_close($conn);
 //                      "Contact"=>"id_tag6");
 //
 echo '<nav class="cl_topmenu cl_centre"><ul class="cl_topmenu">';
-$arrlenght=count($ar_menuitemname)
-for($x=0;$x<$arrlength;$x++)
-  {
-      echo '<li><a href="#id'.$ar_menuitemname[$x].'">'.$ar_menuitemname[$x].'</a></li>';
-  }
-echo '</ul></nav>';
-for($x=0;$x<$arrlength;$x++)
-  {
-      echo '<div id="id'.$ar_menuitemname[$x]. '" class="cl_title_mid">'.$ar_menuitemname[$x].'<br><p class="cl_centre">Erwin '.$menu_name.'</p><br></div>';
-      echo '<li><a href="#id'.$ar_menuitemname[$x].'">'.$ar_menuitemname[$x].'</a></li>';
-  }
-foreach($ar_menu_top as $menu_name => $menu_tag){
+$arrlength=count($ar_menuitemname);
+if ($arrlength>0) {
+    for($x=0;$x<$arrlength;$x++)
+    {
+        echo '<li><a href="#id'.$ar_menuitemname[$x].'">'.$ar_menuitemname[$x].'</a></li>';
+    }
+    echo '</ul></nav>';
+    for($x=0;$x<$arrlength;$x++)
+    {
+        echo '<div id="id'.$ar_menuitemname[$x]. '" class="cl_title_mid">'.$ar_menuitemname[$x].'<br><p class="cl_centre">Erwin '.$ar_menuitemname[$x].'</p><br></div>';
+    }
+}
+else {
+    echo "geen menu items gevonden";
+}
+  mysqli_close($conn);
+// foreach($ar_menu_top as $menu_name => $menu_tag){
     // echo '<br><p class="cl_centre">Erwin '.$menu_name.'</p><br>';
 
-}
+// }
 ?>
