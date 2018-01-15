0740cd0cc7dc099637fe7ba922c62a2c46478c81
Erwin Oudgenoeg <erwin@oudgenoeg.nl>
2018-01-15 16:19:58 +0100

    added matrialize

--- index.php
+++ index.php
@@ -16,30 +16,38 @@ if(!isset($_SESSION)){
         <meta charset="utf-8">
         <link rel="stylesheet" href="./css/style.css">
         <link rel="stylesheet" href="./css/hint.css">
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="./css/materialize.min.css"  media="screen,projection"/>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="icon" href="./favicon.ico" type="image/x-icon">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS -->
-        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
     </head>
 
     <body class="cl_mainarticle" >
 
         <?php
-        include "./php/track.php";
-        include "./php/topmenu.php";
+        ini_set('display_errors', 'On');
+        error_reporting(E_ALL | E_STRICT);
+        // Start the session
+        if(!isset($_SESSION)){
+          session_start();
+        }
+        include "./php/track.php"; // some tracking
+        require_once("./php/funclib.php"); //load my fuction library
+        require_once("./php/topmenu.class.php"); // load class of my top menu
+        $topmenu = new TopMenu(); // create topmenu object
+        // echo "<script>var topmenu =".$topmenu->itemNumberJSON().";</script>";
+        $topmenu->createMenu();
+
         ?>
 
     </body>
-    <!-- <script src="https:://platform.linkedin.com/in.js" type="text/javascript"></script> -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
-    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script> -->
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script> -->
-    <script
-			  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-			  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
-			  crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js"> </script>
+    <script type="text/javascript" src="./js/materialize.min.js"></script>
     <script src="./js/script.js"></script>
 
 </html>
--- php/mitem6.php
+++ php/mitem6.php
@@ -3,6 +3,8 @@
 if(!isset($_SESSION)){
   session_start();
 }
+require_once("./funclib.php");
+
 if (!isset($_SESSION["suc6"])) {
 $_SESSION["Err"] = "";
 $_SESSION["suc6"] = "";
@@ -18,30 +20,40 @@ $_SESSION["mailkeep"] = "";
 <!-- Portfolio voor codegorilla -->
 <!-- Portfolio van en door Erwin Oudgenoeg  -->
 <!-- copyrights  E.A. Oudgenoeg-->
-
-<div class="ww">
-  <p><span class="error">* required field.</span></p>
-  <form action="./php/ww_action.php" method="post">
-    <input name="username" type="text" autofocus  title="Mag alleen uit karakters en spaties bestaan" placeholder="Uw naam.." value="<?php echo  $_SESSION["namekeep"];?>">
-    <span class="error">*</span><br/>
-    <input name="emailaddr" type="text"  title="naam@voorbeeld.nl" placeholder="Uw E-mail adres.." value="<?php echo  $_SESSION["mailkeep"];?>">
-    <span class="error">*</span><br/>
-    <input name="userpw1" type="password" placeholder="Uw wachtwoord.." >
-    <span class="error">*</span><br/>
-    <input name="userpw2" type="password" placeholder="herhaal Uw wachtwoord.." >
-    <span class="error">*</span><br/>
-    <input class="cl_submit" type="submit" value="Verstuur" />
-    <span hidden>*</span>
-  </form>
-  <?php
-  if (isset($_SESSION["Err"])) {
-    echo "<br><span class='error'> ";
-    echo $_SESSION["Err"];
-    echo "</span>";
-  }
-  if (isset($_SESSION["suc6"])) {
-    echo "<br>";
-    echo $_SESSION["suc6"];
-  }
-  ?>
+<!-- form met materialize -->
+<div class="container">
+  <div class="row">
+    <form class="col s12" action="./php/ww_action.php" method="post">
+      <div class="row">
+        <div class="input-field col s6">
+          <input name="username" id="username" type="text" autofocus  title="Mag alleen uit karakters en spaties bestaan" placeholder="Uw naam.." value="<?php echo  $_SESSION["namekeep"];?>">
+          <label for="username">Naam</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="emailadress" name="emailaddr" type="text" title="naam@voorbeeld.nl" placeholder="Uw E-mail adres.." value="<?php echo  $_SESSION["mailkeep"];?>">
+          <label for="emailadress">Email Adres</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="pw1" name="userpw1" type="password" placeholder="Uw wachtwoord.." >
+          <label for="pw1">Password</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="pw2" name="userpw2" type="password" placeholder="herhaal Uw wachtwoord.." >
+          <label for="pw2">Herhaal Password</label>
+        </div>
+        <button class="waves-effect waves-light btn" ><input type="submit" value="Verstuur" /><i class="material-icons right">send</i></button>
+      </div>
+    </form>
+    <?php
+    if (isset($_SESSION["Err"])) {
+      echo "<br><span class='error'> ";
+      echo $_SESSION["Err"];
+      echo "</span>";
+    }
+    if (isset($_SESSION["suc6"])) {
+      echo "<br>";
+      echo $_SESSION["suc6"];
+    }
+    ?>
+  </div>
 </div>
