208687c76a08d02bf7f9f21c792b93bbc0a672ff
Erwin Oudgenoeg <erwin@oudgenoeg.nl>
2018-01-15 16:18:54 +0100

    certificate objects

---
+++ php/certificate.class.php
@@ -0,0 +1,29 @@
+<?php
+include "./php/dbvars.php";
+/**
+ * Class for creating a object with list of my certificates
+ */
+class Certificate # extends AnotherClass
+{
+  protected $certid;
+  protected $certname;
+  protected $certdate;
+  protected $certlink;
+  protected $company;
+  protected $img_name;
+
+  function __construct($certid, $certname, $certdate, $certlink, $company, $img_name) {
+    global $conn;
+    $this->certid = $certid;
+    $this->certname = $certname;
+    $this->certdate = $certdate;
+    $this->certlink = $certlink;
+    $this->company = $company;
+    $this->img_name = $img_name;
+  }
+  function certname() {
+    return $this->certname;
+  }
+
+}
+ ?>
---
+++ php/certificateslist.class.php
@@ -0,0 +1,39 @@
+<?php
+include "./php/dbvars.php";
+require_once("./php/certificate.class.php"); // load class of a certificate
+/**
+ * Class for creating a object with list of my certificates
+ */
+class CertificatesList # extends AnotherClass
+{
+  protected $certificates;
+
+  function __construct() {
+    global $conn;
+    $this->certificates = array();
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = ("SELECT certid, certname, certdate, certlink, company , img_name
+            FROM certificates
+            ORDER BY certdate")
+            ;
+    $result = $conn->query($sql);
+    $i = 0;
+    // fill array with items fom database
+    while($row = $result->fetch_assoc() ) {
+      $this->certificates[$i] =
+        new Certificate($row['certid'], $row['certname'], $row['certdate'],
+        $row['certlink'],  $row['company'], $row['img_name']);
+      $i++;
+    }
+    // close database
+    mysqli_close($conn);
+
+  }
+
+}
+ ?>
